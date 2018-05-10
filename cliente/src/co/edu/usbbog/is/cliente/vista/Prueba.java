e327471af82c90b06cf40e340d2535a15304a6de
204 <204@204equipo25.informatica.usbbog.edu.co>
2018-05-10 17:22:52 -0500

    mensaje

--- cliente/src/co/edu/usbbog/is/cliente/vista/Prueba.java
+++ cliente/src/co/edu/usbbog/is/cliente/vista/Prueba.java
@@ -5,10 +5,31 @@
  */
 package co.edu.usbbog.is.cliente.vista;
 
+import co.edu.usbbog.is.cliente.modelo.Operacion;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  *
  * @author 204
  */
 public class Prueba {
-    
+    public final static int PUERTO_BASICAS = 9990;
+    public final static String HOST_BASICAS = "127.0.0.1";
+    public static void main(String[] args) {
+        Operacion operacion = new Operacion(8.2, 3.0, '-');
+        Socket clienteSocket = null;
+        ObjectOutputStream oos = null;
+        try {
+            clienteSocket = new Socket(HOST_BASICAS, PUERTO_BASICAS);
+            oos = new ObjectOutputStream(clienteSocket.getOutputStream());
+            oos.writeObject(operacion);
+            oos.flush();
+            oos.close();
+        } catch (IOException e) {
+            
+        }
+        
+    }
 }
