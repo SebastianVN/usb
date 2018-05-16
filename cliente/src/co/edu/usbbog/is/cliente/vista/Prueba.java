8b0f09f38145f1796241d67b61774880cada3da8
jofa7 <jofa7@DESKTOP-J3FDD83>
2018-05-16 12:12:57 -0500

    Listo el paquete cliente, con su funcionalidad, falta la conexion con el socket

--- cliente/src/co/edu/usbbog/is/cliente/vista/Prueba.java
+++ cliente/src/co/edu/usbbog/is/cliente/vista/Prueba.java
@@ -15,14 +15,14 @@ import java.net.Socket;
  * @author 204
  */
 public class Prueba {
-    public final static int PUERTO_BASICAS = 9990;
-    public final static String HOST_BASICAS = "127.0.0.1";
+    public final static int PUERTO_POTENCIA = 9992;
+    public final static String HOST_POTENCIA = "127.0.0.1";
     public static void main(String[] args) {
         Operacion operacion = new Operacion(8.2, 3.0, '+');
         Socket clienteSocket = null;
         ObjectOutputStream oos = null;
         try {
-            clienteSocket = new Socket(HOST_BASICAS, PUERTO_BASICAS);
+            clienteSocket = new Socket(HOST_POTENCIA, PUERTO_POTENCIA);
             oos = new ObjectOutputStream(clienteSocket.getOutputStream());
             oos.writeObject(operacion);
             oos.flush();
