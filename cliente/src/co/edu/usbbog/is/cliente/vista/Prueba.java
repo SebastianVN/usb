859384865e5a34fe03be255b82f710ea4fc32c99
204 <204@204equipo25.informatica.usbbog.edu.co>
2018-05-17 15:56:48 -0500

    mensaje

--- cliente/src/co/edu/usbbog/is/cliente/vista/Prueba.java
+++ cliente/src/co/edu/usbbog/is/cliente/vista/Prueba.java
@@ -7,8 +7,11 @@ package co.edu.usbbog.is.cliente.vista;
 
 import co.edu.usbbog.is.calculadora.Operacion;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Clase que me realiza la operacion que el usuario escoga
@@ -19,33 +22,52 @@ public class Prueba {
 
     /**
      * Variable que me guarda el puerto que se va a utilizar como puente entre
-     * servidor y cliente
+     * servidor y cliente para la operaciones basicas
      */
-    public final static int PUERTO_POTENCIA = 9990;
+    public final static int PUERTO_BASICAS = 9990;
+    /**
+     * Variable que me guarda el puerto que se va a utilizar como puente entre
+     * servidor y cliente para las operaciones de potencia
+     */
+    public final static int PUERTO_POTENCIA = 9992;
+    /**
+     * Variable que me guarda el puerto que se va a utilizar como puente entre
+     * servidor y cliente para las operaciones de division
+     */
+    public final static int PUERTO_DIVISION = 9991;
+    
     /**
      * Variable que me guarda la dirección ip que se va a utilizar como puente
      * entre servidor y cliente
      */
-    public final static String HOST_POTENCIA = "127.0.0.1";
+    public final static String HOST = "127.0.0.1";
 
     /**
      * Metodo principal que envia un objeto al servidor que es una operacion
      *
      * @param args
      */
+    
     public static void main(String[] args) {
-        Operacion operacion = new Operacion(2.0, 3.0, '+', "");
+        Operacion operacion = new Operacion(15.0, 3.0, '/', "");
         Socket clienteSocket = null;
         ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
         // Utilizamos un try and catch para manejar excepciones al utilizar sockets
         try {
-            clienteSocket = new Socket(HOST_POTENCIA, PUERTO_POTENCIA);
+            clienteSocket = new Socket(HOST, PUERTO_DIVISION);
             oos = new ObjectOutputStream(clienteSocket.getOutputStream());
             oos.writeObject(operacion);
+            ois = new ObjectInputStream(clienteSocket.getInputStream());
+            operacion = (Operacion) ois.readObject();
+            System.out.println(operacion.toString());
+            ois.close();
             oos.flush();
             oos.close();
         } catch (IOException e) {
 
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Prueba.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
